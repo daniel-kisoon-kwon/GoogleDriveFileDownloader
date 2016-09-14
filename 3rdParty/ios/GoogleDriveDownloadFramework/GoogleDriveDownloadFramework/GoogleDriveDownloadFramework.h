a8919f81ad5a980f3d0f27149d770de42fd0aeaa
daniel-kisoon-kwon <moxie2ks@gmail.com>
2016-09-14 11:56:58 +0900

    Added 3rd party project - GoogleDriveDownloadFramework

---
+++ 3rdParty/ios/GoogleDriveDownloadFramework/GoogleDriveDownloadFramework/GoogleDriveDownloadFramework.h
@@ -0,0 +1,19 @@
+//
+//  GoogleDriveDownloadFramework.h
+//  GoogleDriveDownloadFramework
+//
+//  Created by daniel-kisoon-kwon on 2016. 9. 18..
+//  Copyright © 2016년 daniel-kisoon-kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleDriveDownloadFramework.
+FOUNDATION_EXPORT double GoogleDriveDownloadFrameworkVersionNumber;
+
+//! Project version string for GoogleDriveDownloadFramework.
+FOUNDATION_EXPORT const unsigned char GoogleDriveDownloadFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleDriveDownloadFramework/PublicHeader.h>
+
+
